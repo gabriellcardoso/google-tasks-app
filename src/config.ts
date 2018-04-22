ac32ffa56f964279f35ad0a9fc6c94c3c19c90fe
Gabriel Cardoso <gabriel.cardoso@way2.com.br>
2018-04-22 19:27:37 -0300

    Removing my google api credentials

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 class GoogleApiConfig {
-    static apiKey = 'AIzaSyAx2n0CQ2wlaZc8S7RltYMi-CkbNgPkfVs';
+    static apiKey = '{YOUR API KEY}';
     static discoveryDocs = ['https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest'];
-    static clientId = '951862557912-5bq1p1tui5sj0c8v1nccuuld33h16p2a.apps.googleusercontent.com';
+    static clientId = '{YOUR CLIENT ID}.apps.googleusercontent.com';
     static scope = 'https://www.googleapis.com/auth/tasks';
 }
 
